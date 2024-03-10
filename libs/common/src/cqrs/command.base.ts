faef45820140210d5b41bf9215cd0c09ec7aba8f
Saba Pochkhua <spochkhua@velitech.com>
2024-03-10 16:28:13 +0400

    fix(libs/common): remove utility for cqrs

--- libs/common/src/cqrs/command.base.ts
+++
@@ -1,7 +0,0 @@
-import { ICommand } from '@nestjs/cqrs';
-
-export class BaseCommand<T> implements ICommand {
-  constructor(data: T) {
-    Object.assign(this, data);
-  }
-}
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,4 +1,3 @@
 export * from './dtos';
-export * from './cqrs';
 export * from './exceptions';
 export * from './typing';
