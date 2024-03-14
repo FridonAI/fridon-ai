a27fc3216c92b782442202c0e3568f5d9c53dd26
Saba Pochkhua <spochkhua@velitech.com>
2024-03-14 20:19:27 +0400

    feat(events): implement client getter from auth token

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,47 +1,45 @@
 import {
   OnGatewayConnection,
-  OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { Injectable } from '@nestjs/common';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 @Injectable()
-export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  private sockets: Map<string, Socket[]> = new Map();
+export class EventsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
 
   async handleConnection(client: Socket) {
-    const { walletId } = this.getClientInfoFromAuth(client);
+    try {
+      const { walletId } = this.getClientInfoFromAuth(client);
 
-    const key = walletId;
-    if (!this.sockets.has(key)) {
-      this.sockets.set(key, []);
-    }
-
-    this.sockets.get(key)?.push(client);
-  }
-
-  async handleDisconnect(client: Socket) {
-    const { walletId } = this.getClientInfoFromAuth(client);
-
-    const key = walletId;
-    const sockets = this.sockets.get(key);
-    if (sockets) {
-      const index = sockets.indexOf(client);
-      if (index > -1) {
-        sockets.splice(index, 1);
-      }
+      client.join(walletId);
+    } catch (e) {
+      setTimeout(() => {
+        client._error('Unauthorized');
+        client.disconnect();
+      }, 1000);
     }
   }
 
   public sendTo(walletId: string, event: string, data: any) {
-    this.sockets.get(walletId)?.forEach((socket) => socket.emit(event, data));
+    this.server.to(walletId).emit(event, data);
   }
 
-  private getClientInfoFromAuth(_: Socket): { walletId: string } {
+  private getClientInfoFromAuth(socket: Socket): { walletId: string } {
+    const sessionCookie = socket.handshake.headers.cookie
+      ?.split('; ')
+      .find((cookie: string) => cookie.startsWith('authorization'))
+      ?.split('=')[1];
+
+    if (!sessionCookie) throw new WsException('Unauthorized');
+
     return {
-      walletId: 'RANDOM_WALLET_ID',
+      walletId: sessionCookie!,
     };
   }
 }
