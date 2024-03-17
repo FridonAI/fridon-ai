6b848a5afe003742a6f1b87d913889d50c71d1ab
Saba Pochkhua <spochkhua@velitech.com>
2024-03-18 02:27:16 +0400

    feat(chat): add logging for ai events controller

--- src/chat/external/ai/ai.controller.ts
+++ src/chat/external/ai/ai.controller.ts
@@ -1,14 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { AiChatMessageResponseGeneratedDto } from './ai.dto';
 import { EventsService } from 'src/events/events.service';
 
+const eventName = 'response_received';
+
 @Controller()
 export class AiEventsController {
+  private logger = new Logger(AiEventsController.name);
   constructor(private readonly eventsService: EventsService) {}
 
-  @EventPattern('response_received')
+  @EventPattern(eventName)
   accumulate(data: AiChatMessageResponseGeneratedDto): void {
+    this.logger.debug(
+      `Received event[${eventName}] from AI: ${JSON.stringify(data, null, 2)}`,
+    );
+    this.logger.debug(
+      `Sending response[${data.data.message}] to user[${data.user.walletId}]`,
+    );
     this.eventsService.sendTo(
       data.user.walletId,
       'chat.response-generated',
