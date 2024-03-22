347294e64a32e187e69cf1955a8397dd36be1534
Saba Pochkhua <spochkhua@velitech.com>
2024-03-22 04:57:38 +0400

    fix(chats): not saving response from ai

--- src/chat/external/ai/ai.controller.ts
+++ src/chat/external/ai/ai.controller.ts
@@ -3,6 +3,10 @@ import { EventPattern } from '@nestjs/microservices';
 import { AiChatMessageResponseGeneratedDto } from './ai.dto';
 import { EventsService } from 'src/events/events.service';
 import { BaseDto } from '@lib/common';
+import { ChatRepository } from 'src/chat/chat.repository';
+import { ChatId } from 'src/chat/domain/chat-id.value-object';
+import { ChatMessageId } from 'src/chat/domain/chat-message-id.value-object';
+import { randomUUID } from 'crypto';
 
 const eventName = 'response_received';
 
@@ -14,15 +18,24 @@ export class ChatResponseGeneratedDto extends BaseDto<ChatResponseGeneratedDto> 
 @Controller()
 export class AiEventsController {
   private logger = new Logger(AiEventsController.name);
-  constructor(private readonly eventsService: EventsService) {}
+
+  constructor(
+    private readonly eventsService: EventsService,
+    private readonly chatRepository: ChatRepository,
+  ) {}
 
   @EventPattern(eventName)
-  accumulate(event: AiChatMessageResponseGeneratedDto): void {
+  async process(event: AiChatMessageResponseGeneratedDto): Promise<void> {
     this.logger.debug(
       `Received event[${eventName}] from AI: ${JSON.stringify(event, null, 2)}`,
     );
     this.logger.debug(
       `Sending response[${event.data.message}] to user[${event.user.walletId}]`,
+    );
+    await this.chatRepository.createChatMessageResponse(
+      new ChatId(event.chatId),
+      new ChatMessageId(randomUUID()),
+      event.data.message,
     );
     this.eventsService.sendTo(
       event.user.walletId,
