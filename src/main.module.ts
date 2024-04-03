65f9e52a8a3500448779c79e668515ab31acfb8b
Saba Pochkhua <spochkhua@velitech.com>
2024-04-03 16:09:16 +0400

    chore: remove prisma logs

--- src/main.module.ts
+++ src/main.module.ts
@@ -19,14 +19,7 @@ import { UserModule } from './medias/medias.module';
     // Nest Modules
     CqrsModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule.forRoot({
-      isGlobal: true,
-      prismaServiceOptions: {
-        prismaOptions: {
-          log: ['info', 'query', 'warn', 'error'],
-        },
-      },
-    }),
+    PrismaModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     CacheModule.register({ isGlobal: true }),
     BullModule.forRoot({
