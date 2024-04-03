1e188a7432cb53c1d7f7be1a5996baa6340185bd
Saba Pochkhua <spochkhua@velitech.com>
2024-04-04 02:15:56 +0400

    refactor: cron job runs every 1 hour

--- src/blockchain/cron/update-embedings.cron.ts
+++ src/blockchain/cron/update-embedings.cron.ts
@@ -1,72 +1,16 @@
-import pgvector from 'pgvector';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { readFileSync } from 'fs';
-import { PrismaService } from 'nestjs-prisma';
-import { HuggingFaceAdapter } from '../external/hugging-face.adapter';
-import { BirdEyeAdapter } from '../external/bird-eye.adapter';
+import { CoinSimilarityService } from '../services/coin-similarity';
 
 @Injectable()
 export class UpdateEmbeddings {
   private readonly l = new Logger(UpdateEmbeddings.name);
 
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly huggingFaceAdapter: HuggingFaceAdapter,
-    private readonly birdEyeAdapter: BirdEyeAdapter,
-  ) {}
+  constructor(private readonly coinSimilarityService: CoinSimilarityService) {}
 
-  @Cron(CronExpression.EVERY_5_SECONDS)
+  @Cron(CronExpression.EVERY_HOUR)
   async execute() {
     this.l.debug('Called Update Embeddings Cron Job');
-
-    const tokenAddresses = this.getTokenAddresses();
-
-    const arr: number[][] = [];
-    for (const token of tokenAddresses) {
-      const data = await this.getBirdEyeData(token.address);
-      arr.push(data);
-    }
-
-    const result = await this.huggingFaceAdapter.getEmbeddings(arr);
-    const richResult = result.vector.map((vector, index) => ({
-      vector,
-      symbol: tokenAddresses[index]!.symbol,
-      address: tokenAddresses[index]!.address,
-      values: arr[index]!,
-    }));
-
-    result.vector.forEach(async (vector, i) => {
-      const embedding = pgvector.toSql(vector);
-      const values = pgvector.toSql(richResult[i]!.values);
-      await this.prisma.$executeRaw`
-          INSERT INTO price_vectors 
-          (embedding, symbol, address, values) 
-          VALUES
-          (${embedding}::vector,${richResult[i]!.symbol}, ${richResult[i]!.address}, ${values}::vector)`;
-    });
-  }
-
-  getTokenAddresses(): {
-    symbol: string;
-    address: string;
-  }[] {
-    const res = readFileSync('./src/blockchain/cron/coins-list.json', 'utf-8');
-    return JSON.parse(res);
-  }
-
-  async getBirdEyeData(tokenAddress: string): Promise<number[]> {
-    const timeTo = new Date().getTime();
-    const timeFrom = timeTo - 60 * 60 * 24 * 30;
-    const data = await this.birdEyeAdapter.getHistoryPrice(
-      tokenAddress,
-      timeFrom,
-      timeTo,
-    );
-
-    while (data.length < 512) {
-      data.unshift(0);
-    }
-    return data;
+    await this.coinSimilarityService.updateEmbeddings();
   }
 }
--- src/blockchain/services/coin-similarity.ts
+++ src/blockchain/services/coin-similarity.ts
@@ -1,16 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import pgvector from 'pgvector';
 import { HuggingFaceAdapter } from '../external/hugging-face.adapter';
 import { BirdEyeAdapter } from '../external/bird-eye.adapter';
+import { readFileSync } from 'fs';
 
 @Injectable()
-export class CoinSimilarityService {
+export class CoinSimilarityService implements OnApplicationBootstrap {
+  private readonly l = new Logger(CoinSimilarityService.name);
+
   constructor(
     private readonly prisma: PrismaService,
     private readonly huggingFaceAdapter: HuggingFaceAdapter,
     private readonly birdEyeAdapter: BirdEyeAdapter,
   ) {}
+
+  async onApplicationBootstrap() {
+    this.l.debug('Application Bootstrap: Updating Embeddings');
+    await this.updateEmbeddings();
+    this.l.debug('Application Bootstrap: Embeddings Updated');
+  }
+
+  async updateEmbeddings() {
+    const tokenAddresses = this.getTokenAddresses();
+
+    const arr: number[][] = [];
+    for (const token of tokenAddresses) {
+      const data = await this.getBirdEyeData(token.address);
+      arr.push(data);
+    }
+
+    const result = await this.huggingFaceAdapter.getEmbeddings(arr);
+    const richResult = result.vector.map((vector, index) => ({
+      vector,
+      symbol: tokenAddresses[index]!.symbol,
+      address: tokenAddresses[index]!.address,
+      values: arr[index]!,
+    }));
+
+    result.vector.forEach(async (vector, i) => {
+      const embedding = pgvector.toSql(vector);
+      const values = pgvector.toSql(richResult[i]!.values);
+      await this.prisma.$executeRaw`
+          INSERT INTO price_vectors 
+          (embedding, symbol, address, values) 
+          VALUES
+          (${embedding}::vector,${richResult[i]!.symbol}, ${richResult[i]!.address}, ${values}::vector)`;
+    });
+  }
 
   async getCoinSimilarity(symbol: string, from: number, to: number, k: number) {
     const data = await this.birdEyeAdapter.getHistoryPrice(symbol, from, to);
@@ -30,5 +67,28 @@ export class CoinSimilarityService {
         WHERE address != ${symbol}
         ORDER BY 1 - (embedding <=> ${vector}::vector) DESC LIMIT ${k}`;
     return res;
+  }
+
+  private getTokenAddresses(): {
+    symbol: string;
+    address: string;
+  }[] {
+    const res = readFileSync('./src/blockchain/cron/coins-list.json', 'utf-8');
+    return JSON.parse(res);
+  }
+
+  private async getBirdEyeData(tokenAddress: string): Promise<number[]> {
+    const timeTo = new Date().getTime();
+    const timeFrom = timeTo - 60 * 60 * 24 * 30;
+    const data = await this.birdEyeAdapter.getHistoryPrice(
+      tokenAddress,
+      timeFrom,
+      timeTo,
+    );
+
+    while (data.length < 512) {
+      data.unshift(0);
+    }
+    return data;
   }
 }
