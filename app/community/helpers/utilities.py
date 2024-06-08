3f7d2b872253ba37b1ce41202cc404133a4e1082
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-06-09 01:59:14 +0400

    feat: create helper utils

---
+++ app/community/helpers/utilities.py
@@ -0,0 +1,25 @@
+from pydantic.v1 import BaseModel, Field
+
+from app.core.plugins.utilities.llm import LLMUtility
+
+
+class DatetimeExtractorUtility(LLMUtility):
+    class DatetimeExtractorOutput(BaseModel):
+        """Datetime string with iso format"""
+        datetime: str = Field(escription="Valid iso format datetime")
+
+    llm_job_description = """You are a datetime extractor from user's input to the chat assistant. \
+Return datetime in iso format like: 2022-01-01"
+User input: {user_input}
+"""
+    structured_output = DatetimeExtractorOutput
+
+    async def _arun(
+            self,
+            user_input: str,
+            *args,
+            **kwargs
+    ) -> dict:
+        return {
+            "user_input": user_input,
+        }
