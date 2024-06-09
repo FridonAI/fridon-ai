c6185e89920c0bf5d6613d6d5c94448b5699da26
Saba Pochkhua <spochkhua@velitech.com>
2024-06-09 19:42:28 +0400

    refactor: leaderboard return all data

--- src/chat/leaderboard.controller.ts
+++ src/chat/leaderboard.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { LeaderBoardService } from './leaderboard.service';
 import { LeaderboardResponseDto } from './leaderboard.dto';
+import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 
 @Controller('leaderboard')
 @ApiTags('leaderboard')
@@ -10,6 +11,8 @@ export class LeaderboardHttpController {
   constructor(private readonly leaderboardService: LeaderBoardService) {}
 
   @Get()
+  @UseInterceptors(CacheInterceptor)
+  @CacheTTL(5) // ToDo: Change to 30 seconds
   @ApiOkResponse({ type: LeaderboardResponseDto })
   async getChats(): Promise<LeaderboardResponseDto> {
     return await this.leaderboardService.getLeaderboard();
