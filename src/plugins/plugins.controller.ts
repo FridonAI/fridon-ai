c0cdcb6a394ba2ce517da8be1873bf481cf32ab9
Saba Pochkhua <spochkhua@velitech.com>
2024-06-09 22:37:03 +0400

    feat(plugins): add subscribers count

--- src/plugins/plugins.controller.ts
+++ src/plugins/plugins.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { PluginsService } from './plugins.service';
 import { ApiTags } from '@nestjs/swagger';
+import { FindAllPluginsDtoResponseDto } from './plugins.dto';
+import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 
 @Controller('plugins')
 @ApiTags('plugins')
+@UseInterceptors(CacheInterceptor)
 export class PluginsController {
   constructor(private readonly pluginService: PluginsService) {}
 
+  @CacheTTL(5) // ToDo: Change to 30 seconds
   @Get()
-  findAll() {
-    return this.pluginService.getPlugins();
+  async findAll(): Promise<FindAllPluginsDtoResponseDto[]> {
+    const plugins = this.pluginService.getPlugins();
+
+    const res = Promise.all(
+      plugins.map(async (plugin) => ({
+        ...plugin,
+        subscribersCount: await this.pluginService.getSubscribersCount(
+          plugin.slug,
+        ),
+      })),
+    );
+
+    return res;
   }
 }
