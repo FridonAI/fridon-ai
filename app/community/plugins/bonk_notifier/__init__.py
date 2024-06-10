39296f45ce0e7a23cf8bbf037b76818736fa9b20
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-06-10 22:45:21 +0400

    refactor: add image urls

--- app/community/plugins/bonk_notifier/__init__.py
+++ app/community/plugins/bonk_notifier/__init__.py
@@ -11,3 +11,4 @@ class BonkBullishNotifierPlugin(BasePlugin):
     owner = "2snYEzbMckwnv85MW3s2sCaEQ1wtKZv2cj9WhbmDuuRD"
     price = 1000
     tools: type[list[BaseTool]] = []
+    image_url = "https://fridon-ai-assets.s3.eu-central-1.amazonaws.com/agent-avatars/bonkheadog.png"
