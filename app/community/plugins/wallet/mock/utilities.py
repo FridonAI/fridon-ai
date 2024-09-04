cbde600e851e95f9d14d69591e7c48190983d7f8
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-09-05 01:56:32 +0400

    refactor: diversify mock utility responses

--- app/community/plugins/wallet/mock/utilities.py
+++ app/community/plugins/wallet/mock/utilities.py
@@ -11,10 +11,14 @@ class WalletTransferMockUtility(BlockchainMockUtility):
             *args,
             **kwargs
     ) -> str:
-        return f"{amount} {currency} successfully transfered from {wallet_id} to {to_wallet_address}"
+        return f"{amount} {currency} successfully transferred from {wallet_id} to {to_wallet_address}"
 
 
 class WalletBalanceMockUtility(RemoteMockUtility):
+    wallet_balances = {
+        "sol": 23,
+        "eth": 1.2,
+    }
     async def arun(
             self,
             *args,
@@ -22,6 +26,10 @@ class WalletBalanceMockUtility(RemoteMockUtility):
             wallet_id: str = "Wallet007007",
             **kwargs
     ) -> dict:
-        return {"data": {"sol": 23}}
+
+        if currency is None:
+            return {"data": self.wallet_balances}
+        amount = self.wallet_balances.get(currency, 0)
+        return {"data": {currency: amount}}
 
 
