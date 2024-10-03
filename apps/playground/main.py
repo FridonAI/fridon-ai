84a448b7b782121c5ac956cf8ca3fd8ead75865c
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-10-04 02:00:26 +0400

    refactor: change response generation callings

--- apps/playground/main.py
+++ apps/playground/main.py
@@ -1,8 +1,6 @@
 from langchain_openai.chat_models import ChatOpenAI
-from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
-from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
 
-from libs.core.graph import create_graph
+from libs.core.graph.base import generate_response
 from libs.core.plugins.registry import ensure_plugin_registry
 from apps.fridon_brain import _preload_modules
 
@@ -16,31 +14,22 @@ async def main():
     plugins = [registry.plugins[plugin_name]() for plugin_name in plugin_names]
 
     llm = ChatOpenAI(model="gpt-4o", temperature=0, api_key=settings.OPENAI_API_KEY, verbose=True)
-
-    async with AsyncPostgresSaver.from_conn_string(settings.POSTGRES_DB_URL, pipeline=False) as saver:
-        print(saver)
-        await saver.setup()
-        graph = create_graph(llm, plugins, memory=saver)
-        print(graph.checkpointer)
-        config = {  
-            "configurable": {
-                "thread_id": "1",
-                "wallet_id": "2snYEzbMckwnv85MW3s2sCaEQ1wtKZv2cj9WhbmDuuRD",
-                "chat_id": "bla"
-            }
+    config = {  
+            "thread_id": "1",
+            "wallet_id": "2snYEzbMckwnv85MW3s2sCaEQ1wtKZv2cj9WhbmDuuRD",
+            "chat_id": "bla"
         }
-
-        async for s in graph.astream(
-                {
-                    "messages": [
-                        "who are you?",
-                    ],
-                },
-                config,
-                stream_mode="values"
-        ):
-            if "__end__" not in s:
-                s["messages"][-1].pretty_print()
+    
+    final_response, used_agents = await generate_response(
+        "What's my balance?",
+        plugins,
+        llm,
+        config,
+        memory="postgres"
+        )
+    
+    print(final_response)
+    print(used_agents)
 
 
 if __name__ == "__main__":
