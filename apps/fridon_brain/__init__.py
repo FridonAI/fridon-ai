bdbe8ded424ee836e6c187b361ecbc4ff49cc249
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-10-05 00:00:44 +0400

    fix: errors

--- apps/fridon_brain/__init__.py
+++ apps/fridon_brain/__init__.py
@@ -1,7 +1,7 @@
 def _preload_modules():
     print("load")
     import libs.community.plugins
-    from fridonai_core.module_utils import resolve_module_plugins
+    from libs.core.module_utils import resolve_module_plugins
 
     resolve_module_plugins(libs.community.plugins)
 
