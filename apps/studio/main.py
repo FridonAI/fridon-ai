edab67311e6354dc99322194023edcf93da825d6
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-10-15 01:06:11 +0400

    refactor: change langgraph studion code

--- apps/studio/main.py
+++ apps/studio/main.py
@@ -1,3 +1,4 @@
+from fridonai_core.graph.models import get_model
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.sqlite import SqliteSaver
 
@@ -7,32 +8,21 @@ from settings import settings
 from apps.fridon_brain import _preload_modules
 
 
-def _prepare_graph(plugin_names):
-    registry = ensure_plugin_registry()
-    plugins = [registry.plugins[plugin_name]() for plugin_name in plugin_names]
-    plugins = [p for p in plugins if p.exclude is False]
-
-    llm = ChatOpenAI(
-        model="gpt-4o",
-        temperature=0,
-        openai_api_key=settings.OPENAI_API_KEY,
-        verbose=True,
-    )
-    with SqliteSaver.from_conn_string(":memory:") as saver:
-        graph = create_graph(llm, plugins, memory=saver)
-    return graph
-
-
 def test_chat():
     _preload_modules()
-
     plugin_names = [
         "wallet",
         "fridon",
         "coin-technical-analyzer",
         "coin-price-chart-similarity-search",
-        "jupiter",
     ]
-    graph = _prepare_graph(plugin_names)
+    registry = ensure_plugin_registry()
+    plugins = [registry.plugins[plugin_name]() for plugin_name in plugin_names]
+    plugins = [p for p in plugins if p.exclude is False]
+
+    llm = get_model()
+
+    with SqliteSaver.from_conn_string(":memory:") as saver:
+        graph = create_graph(llm, plugins, memory=saver)
 
     return graph
