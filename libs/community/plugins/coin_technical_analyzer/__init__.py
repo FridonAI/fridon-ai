1c24bc8933f81cac443a4f0a039cee178bda6145
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-10-20 02:05:33 +0400

    refactor: change plugin description

--- libs/community/plugins/coin_technical_analyzer/__init__.py
+++ libs/community/plugins/coin_technical_analyzer/__init__.py
@@ -9,7 +9,7 @@ plugin_registry = ensure_plugin_registry()
 @plugin_registry.register(name="coin-technical-analyzer")
 class CoinsTechnicalAnalyzerPlugin(BasePlugin):
     name: str = "Coin Technical Analyzer"
-    description: str = "AI plugin for analyze coin price charts by indicators, search coins by indicators."
+    description: str = "AI plugin for analyzing and plotting coin price charts with technical indicators."
     owner: str = "2snYEzbMckwnv85MW3s2sCaEQ1wtKZv2cj9WhbmDuuRD"
     tools: list[BaseTool] = TOOLS
     image_url: str = "https://fridon-ai-assets.s3.eu-central-1.amazonaws.com/agent-avatars/coin-ta.png"
