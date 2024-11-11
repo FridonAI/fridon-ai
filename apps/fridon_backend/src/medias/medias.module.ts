868975f290208a21e19c892e0c7725075c5d2296
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-11-11 20:54:37 +0400

    refactor: delete media redundant stuff

--- apps/fridon_backend/src/medias/medias.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MediasService } from './medias.service';
-import { MediaController } from './medias.controller';
-
-@Module({
-  controllers: [MediaController],
-  providers: [MediasService],
-})
-export class MediaModule {}
