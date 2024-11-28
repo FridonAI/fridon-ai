180cf06c58b4238e0a0b1abc3fe9228b0e873e3b
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-11-29 00:00:31 +0400

    refator: edit for start and end time extraction

--- libs/community/helpers/utilities.py
+++ libs/community/helpers/utilities.py
@@ -6,10 +6,18 @@ from fridonai_core.plugins.utilities.llm import LLMUtility
 class DatetimeExtractorUtility(LLMUtility):
     class DatetimeExtractorOutput(BaseModel):
         """Datetime string with iso format"""
-        datetime: str = Field(description="Valid iso format datetime")
 
-    llm_job_description: str = """You are a datetime extractor from user's input to the chat assistant. \
-Return datetime in iso format like: 2022-01-01"
+        starting_time: str | None = Field(
+            default=None,
+            description="Valid iso format datetime which is starting time of a period",
+        )
+        ending_time: str | None = Field(
+            default=None,
+            description="Valid iso format datetime which is ending time of a period",
+        )
+
+    llm_job_description: str = """You are a starting and ending datetime extractor from user's input to the chat assistant. \
+Return datetime in iso format like: 2022-01-01 12:00:00"
 User input: {user_input}
 """
     structured_output: type = DatetimeExtractorOutput
