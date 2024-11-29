bfe5ed56608c1232ef246df901a4809f249a08e5
Nika Losaberidze <nikalosaberidze@gmail.com>
2024-11-29 22:05:03 +0400

    refactor: create cron based on env

--- apps/fridon_brain/__init__.py
+++ apps/fridon_brain/__init__.py
@@ -1,12 +1,17 @@
+from settings import settings
+
+
 def _preload_modules():
-    print("load")
     import libs.community.plugins
     from libs.core.module_utils import resolve_module_plugins
 
     resolve_module_plugins(libs.community.plugins)
 
-    import asyncio
+    if settings.ENVIRONMENT != "local":
+        import asyncio
 
-    from fridonai_core.crons.registry import ensure_cron_registry
-    asyncio.get_event_loop().run_until_complete(ensure_cron_registry().start_crons())
+        from fridonai_core.crons.registry import ensure_cron_registry
 
+        asyncio.get_event_loop().run_until_complete(
+            ensure_cron_registry().start_crons()
+        )
