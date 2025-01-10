4fda7500b6b4030a53c95a5d9b197fa0de534ce8
Nika Losaberidze <nikalosaberidze@gmail.com>
2025-01-11 01:37:09 +0400

    feat: make each agent langgraph instance with its state

--- libs/core/fridonai_core/graph/routers.py
+++ libs/core/fridonai_core/graph/routers.py
@@ -10,11 +10,11 @@ def route_plugin_agent(
 ) -> str:
     route = tools_condition(state)
     if route == END:
-        return "supervisor"
+        return END
     tool_calls = state["messages"][-1].tool_calls
     did_cancel = any(tc["name"] == CompleteTool.__name__ for tc in tool_calls)
     if did_cancel:
-        return "leave_tool"
+        return END
     return "tool_node"
 
 
@@ -22,4 +22,4 @@ def route_supervisor_agent(state: State, wrapped_plugins_to_plugins) -> str:
     tool_calls = state["messages"][-1].tool_calls
     if not tool_calls:
         return "respond"
-    return "Enter" + wrapped_plugins_to_plugins[tool_calls[0]["name"]]
+    return wrapped_plugins_to_plugins[tool_calls[0]["name"]]
--- libs/core/fridonai_core/graph/states.py
+++ libs/core/fridonai_core/graph/states.py
@@ -1,13 +1,28 @@
 from operator import add
 from typing import Annotated, List, TypedDict, Union
 
-from langchain_core.messages import AnyMessage
+from langchain_core.messages import AnyMessage, ToolMessage
 from langgraph.graph import add_messages
 
 from fridonai_core.graph.tools import FinalResponse
+from fridonai_core.graph.tools import CompleteTool
 
 
 class State(TypedDict):
     messages: Annotated[List[AnyMessage], add_messages]
     used_agents: Annotated[list[str], add]
     final_response: Union[FinalResponse, None]
+
+class SubState(TypedDict):
+    messages: Annotated[List[AnyMessage], add_messages]
+
+
+def attach_tool_response_to_tool(state: State) -> dict:
+    tool_calls = state["messages"][-1].tool_calls
+    for tc in filter(lambda tc: tc["name"] == CompleteTool.__name__, tool_calls):
+        tool_id = state["messages"][0].tool_calls[0]["id"]
+        return {
+            "messages": [
+                ToolMessage(tool_call_id=tool_id, content=tc["args"], name="Result"),
+            ]
+        }
