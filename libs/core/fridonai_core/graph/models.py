581db72b0fe480fcf99abbbc347529aed87a7fa9
Nika Losaberidze <nikalosaberidze@gmail.com>
2025-01-11 01:34:55 +0400

    feat: add gpt-4o-mini in models

--- libs/core/fridonai_core/graph/models.py
+++ libs/core/fridonai_core/graph/models.py
@@ -8,8 +8,21 @@ chat_gpt_model = ChatOpenAI(
     verbose=True
 )
 
-def get_model(name="gpt"):
-    return chat_gpt_model
+chat_gpt_model_mini = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0,
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    verbose=True,
+)
+
+
+def get_model(name="gpt-4o"):
+    if name == "gpt-4o":
+        return chat_gpt_model
+    elif name == "gpt-4o-mini":
+        return chat_gpt_model_mini
+    else:
+        return chat_gpt_model
 
 
 def create_structured_output_model(output_format, model=chat_gpt_model):
