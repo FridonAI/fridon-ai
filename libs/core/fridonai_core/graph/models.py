1db26d5ea6234c825cbd750a57b61b167ef29279
Nika Losaberidze <nikalosaberidze@gmail.com>
2025-01-30 19:25:29 +0400

    feat: add deepseek and together provider support

--- libs/core/fridonai_core/graph/models.py
+++ libs/core/fridonai_core/graph/models.py
@@ -1,5 +1,7 @@
 import os 
 from langchain_openai import ChatOpenAI
+from langchain_openai.chat_models.base import BaseChatOpenAI
+from langchain_together import ChatTogether
 
 chat_gpt_model = ChatOpenAI(
     model="gpt-4o", 
@@ -15,6 +17,19 @@ chat_gpt_model_mini = ChatOpenAI(
     verbose=True,
 )
 
+deepseek_v3 = BaseChatOpenAI(
+    model="deepseek-chat",
+    openai_api_key="sk-7ed74465fc20437184a61bf5a20229b2",
+    openai_api_base="https://api.deepseek.com",
+    max_tokens=1024,
+)
+
+deepseek_v3_together = ChatTogether(
+    model="deepseek-ai/DeepSeek-V3",
+    temperature=0,
+    together_api_key="6525e1be995840f2a2e13d4d31a476c7aaf7e40d11a0a329693bf677450b8db0",
+)
+
 
 def get_model(name="gpt-4o"):
     if name == "gpt-4o":
@@ -22,7 +37,7 @@ def get_model(name="gpt-4o"):
     elif name == "gpt-4o-mini":
         return chat_gpt_model_mini
     else:
-        return chat_gpt_model
+        return deepseek_v3_together
 
 
 def create_structured_output_model(output_format, model=chat_gpt_model):
